5726a9f94885959877bdfdbfaa38e87aad4be215
matthieu-op <82909009+matthieu-op@users.noreply.github.com>
2022-01-14 00:27:12 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,23 @@ set -e
 set -u
 
 export DEBIAN_FRONTEND=noninteractive
-
+url_output() {
+    NAME=${1}
+    APPBOX_USER=$(echo "${HOSTNAME}" | awk -F'.' '{print $2}')
+    echo -e "\n\n\n\n\n
+        Installation sucessful! Please point your browser to:
+        \e[4mhttps://${HOSTNAME}/${NAME}\e[39m\e[0m
+        
+        You can continue the configuration from there.
+        \e[96mMake sure you protect the app by setting up a username/password in the app's settings!\e[39m
+        
+        \e[91mIf you want to use another appbox app in the settings of ${NAME}, make sure you access it on port 80, and without https, for example:
+        \e[4mhttp://rutorrent.${APPBOX_USER}.appboxes.co\e[39m\e[0m
+        \e[95mIf you want to access Plex from one of these installed apps use port 32400 for example:
+        \e[4mhttp://plex.${APPBOX_USER}.appboxes.co:32400\e[39m\e[0m
+        
+        That's because inside this container, we don't go through the appbox proxy! \n\n\n\n\n\n"
+}
 
 configure_nginx() {
     NAME=up
