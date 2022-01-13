fdf3a260551dec22b722076c39c293c1e689be38
matthieu-op <82909009+matthieu-op@users.noreply.github.com>
2022-01-14 00:12:08 +0100

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,21 @@
+
+
+configure_nginx() {
+    NAME=up
+    PORT=3001
+    OPTION=${3:-default}
+    if ! grep -q "/${NAME} {" /etc/nginx/sites-enabled/default; then
+        sed -i '/server_name _/a \
+        location /'${NAME}' {\
+                proxy_pass http://127.0.0.1:'${PORT}';\
+        }' /etc/nginx/sites-enabled/default
+
+        if [ "${OPTION}" == 'subfilter' ]; then
+            sed -i '/location \/'${NAME}' /a \
+                sub_filter "http://"  "https://";\
+                sub_filter_once off;' /etc/nginx/sites-enabled/default
+        fi
+    fi
+    pkill -HUP nginx
+    url_output "${NAME}"
+}
